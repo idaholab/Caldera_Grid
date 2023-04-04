44151fc2b83240dd5f727725a7d8e3fd250fd825
Steven Schmidt <steven.schmidt@inl.gov>
2023-04-04 16:14:32 -0600

    Updating the job.sh script. [FUSE Milestone Only]

--- job.sh
+++ job.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 #PBS -N FUSE_milestone
 #PBS -l select=1:ncpus=48
@@ -7,14 +7,19 @@
 #PBS -P Caldera
 #PBS -j oe
 
+echo module load conda/init
 module load conda/init
-conda activate Caldera
 
+echo conda activate ${condenvname}
+conda activate ${condenvname}
+
+echo cd $PBS_O_WORKDIR
 cd $PBS_O_WORKDIR
 
 echo ${io}
 
 echo "starting simulation"
+echo python start_execution.py -io ${io}
 python start_execution.py -io ${io}
 echo "done simulation"
 
