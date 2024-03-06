2e690518b032bcfa8cb02e592e3ea9f1c9403675
Manoj Kumar Cebol Sundarrajan <ManojKumar.CebolSundarrajan@inl.gov>
2024-03-05 20:53:58 -0700

    updating start script to run input scenarios

--- start_exe_with_args.py
+++ start_exe_with_args.py
@@ -1,26 +1,42 @@
 import subprocess
 import sys
 import os
+import time
+import glob
+
 path_to_here = os.path.abspath(os.path.dirname(sys.argv[0]))
 
-# Prepare the command and run it.
-path_to_libs = os.path.join( path_to_here, "./" )
-input_directory = os.path.join( path_to_here, "inputs/" )
-output_directory = os.path.join( path_to_here, "outputs/" )
-timestep = 1*60
-starttime = 6*3600
-endtime = 25*3600
+# folders = ["uncontrolled", "linear/choppy", "linear/smooth", "steep_cubic/choppy", "steep_cubic/smooth", "inverse_s/choppy", "inverse_s/smooth"]
 
-command = [
-    "python", os.path.join( path_to_here, "./start_execution.py" ),
-    "-libs", path_to_libs,
-    "-in", input_directory,
-    "-out", output_directory,
-    "-ts", str(timestep),
-    "-start", str(starttime),
-    "-end", str(endtime),
-    "-opendss","False",
-    "-epcnmfecs","True",
-]
-print("Running command:  ",command)
-subprocess.call(command)
+folders = []
+folders.append("inputs/uncontrolled/")
+folders.extend(glob.glob("inputs/good_forecast/*/*/"))
+folders.extend(glob.glob("inputs/bad_forecast/*/*/"))
+
+folders = [folder.replace("inputs/", "") for folder in folders]
+
+for folder in folders:
+
+    # Prepare the command and run it.
+    path_to_libs = os.path.join( path_to_here, "./" )
+    input_directory = os.path.join( path_to_here, "inputs/", folder )
+    output_directory = os.path.join( path_to_here, "outputs/", folder )
+    timestep = 1*60
+    starttime = 1*3600
+    endtime = 60*3600
+
+    command = [
+        "python", os.path.join( path_to_here, "./start_execution.py" ),
+        "-libs", path_to_libs,
+        "-in", input_directory,
+        "-out", output_directory,
+        "-ts", str(timestep),
+        "-start", str(starttime),
+        "-end", str(endtime),
+        "-opendss","False",
+        "-epcnmfecs","True",
+    ]
+    print("Running command:  ",command)
+    subprocess.call(command)
+
+    time.sleep(15)
