a59a85ea3ecf5a904982c35a3b963aca4efc2a95
Manoj Kumar Cebol Sundarrajan <ManojKumar.CebolSundarrajan@inl.gov>
2024-05-13 11:41:21 -0600

    uncontrolled and TOU setup for winds SCM

---
+++ examples/ex_wind/start_exe_with_args.py
@@ -0,0 +1,40 @@
+import subprocess
+import sys
+import os
+import time
+import glob
+
+path_to_here = os.path.abspath(os.path.dirname(sys.argv[0]))
+
+# folders = ["uncontrolled", "linear/choppy", "linear/smooth", "steep_cubic/choppy", "steep_cubic/smooth", "inverse_s/choppy", "inverse_s/smooth"]
+
+folders = []
+folders.extend(glob.glob(os.path.join(path_to_here,"inputs/uncontrolled/")))
+folders.extend(glob.glob(os.path.join(path_to_here,"inputs/time_of_use/")))
+
+for input_folder in folders:
+    print("folder: ",input_folder)
+
+    # Prepare the command and run it.
+    path_to_libs = os.path.join( path_to_here, "../../" )
+    input_directory = input_folder
+    output_directory = input_folder.replace("inputs", "outputs")
+    timestep = 1*60
+    starttime = 12*3600
+    endtime = 60*3600
+    command = [
+        "python", os.path.join( path_to_here, "./start_execution.py" ),
+        "-libs", path_to_libs,
+        "-in", input_directory,
+        "-out", output_directory,
+        "-hel", os.path.join( input_directory, "helics_config" ),
+        "-ts", str(timestep),
+        "-start", str(starttime),
+        "-end", str(endtime),
+        "-opendss","False",
+        "-epcnmfecs","True",
+    ]
+    print("Running command:  ",command)
+    subprocess.call(command)
+
+    time.sleep(5)
