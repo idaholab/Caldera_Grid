ce53973778b40391dda745c49a8fd9b4c0beec8b
Manoj Kumar Cebol Sundarrajan <ManojKumar.CebolSundarrajan@inl.gov>
2024-08-06 15:26:26 -0600

    integrated input generation into run_all.py

--- examples/ex_FUSE/run_all.py
+++ examples/ex_FUSE/run_all.py
@@ -1,10 +1,48 @@
 import subprocess
-import glob
 import os
 import sys
 import time
+import pandas as pd
+import shutil
+import random
+
+#-------------------------------------
+
+MIN_PYTHON = (3, 8)
+if sys.version_info < MIN_PYTHON:
+    sys.exit("Python %s.%s or later is required.\n" % MIN_PYTHON)
+
+#-------------------------------
+#      Helper Functions
+#-------------------------------
+
+def reset_dir(dir_path : str):
+
+    if os.path.exists(dir_path) and os.path.isdir(dir_path):
+        shutil.rmtree(dir_path, ignore_errors=False)
+
+    os.makedirs(os.path.join(dir_path))    # intermediate directory is also created
+
+#-------------------------------
+#      Scenarios
+#-------------------------------
+
+scenarios = []
+scenarios.append("home_uncontrolled")
+scenarios.append("work_uncontrolled")
+scenarios.append("home_dynamic")
+scenarios.append("work_dynamic")
+
+#-------------------------------
+#      Inputs
+#-------------------------------
 
 path_to_here = os.path.abspath(os.path.dirname(sys.argv[0]))
+
+input_path = os.path.join(path_to_here, "inputs")
+output_path = os.path.join(path_to_here, "outputs")
+
+n_samples = 1000
 
 if len(sys.argv) < 2:
     print("Error: The script run_all.py takes 1 argument, and it should be local or HPC depending on the environment being run")
@@ -16,14 +54,78 @@ if sys.argv[1] != "HPC" and sys.argv[1] != "local":
 
 sim_env = sys.argv[1]
 
-sims_to_run = []
-sims_to_run.append(os.path.join("work"))
-#sims_to_run.extend(os.path.join("home_dynamic"))
-#sims_to_run.extend(os.path.join("work_dynamic"))
-#sims_to_run.extend(os.path.join("home_uncontrolled"))
-#sims_to_run.extend(os.path.join("work_uncontrolled"))
+#-------------------------------
+#      Setup input folders
+#-------------------------------
 
-for sim in sims_to_run:
+reset_dir(input_path)       # Clean folder
+
+base_input_folder = os.path.join(path_to_here, "TE_profiles", "base_inputs_folder") # base copy of input dir
+
+for folder in scenarios:
+    
+    input_folder = os.path.join(input_path, folder)
+    
+    # dirs_exist_ok exist only in python 3.8 and above
+    shutil.copytree(base_input_folder, input_folder, dirs_exist_ok = True)  # copy base input folder to input folder
+
+#-------------------------------
+#      Update CE and SE files
+#-------------------------------
+
+CE_df = pd.read_csv(os.path.join(path_to_here, "TE_profiles", "CE_ICM_work_dominant_original.csv"), keep_default_na=False)
+SE_df = pd.read_csv(os.path.join(path_to_here, "TE_profiles", "SE_ICM_work_dominant_original.csv"), keep_default_na=False)
+
+for scenario_name in scenarios:
+    
+    input_folder = os.path.join(input_path, scenario_name)
+    
+    if "home" in scenario_name:
+        range_min = 100000000
+        range_max = 200000000
+    elif "work" in scenario_name:
+        range_min = 200000000
+        range_max = 300000000
+    else:
+        raise ValueError('scenario name should have home or work in it.')
+
+    sub_CE_df = CE_df[(CE_df["charge_event_id"] >= range_min)  & (CE_df["charge_event_id"] < range_max)]
+    sub_CE_df = sub_CE_df.sample(n=n_samples)
+    
+    if "uncontrolled" in scenario_name:
+
+        sub_CE_df["ES_strategy"] = "NA"
+        sub_CE_df["VS_strategy"] = "NA"
+        sub_CE_df["Ext_strategy"] = "NA"
+        
+    elif "dynamic" in scenario_name:
+
+        sub_CE_df["ES_strategy"] = "NA"
+        sub_CE_df["VS_strategy"] = "NA"
+        sub_CE_df["Ext_strategy"] = "ext0001"
+
+    else:
+        raise ValueError('scenario name should have uncontrolled or dynamic in it.')   
+        
+    sub_CE_df.to_csv(os.path.join(input_folder, "CE_{}.csv".format(scenario_name)), index = False)
+    
+    sub_SE_df = SE_df[SE_df["SE_id"].isin(sub_CE_df["SE_id"])]
+    
+    sub_SE_df.to_csv(os.path.join(input_folder, "SE_{}.csv".format(scenario_name)), index = False)
+
+
+#-------------------------------
+#      Setup output folders
+#-------------------------------
+
+reset_dir(output_path)      # Clean folder
+
+for folder in scenarios:
+    output_folder = os.path.join(output_path, folder)
+    reset_dir(output_folder)        # reset also creates empty dir
+
+
+for sim in scenarios:
 
     if sim_env == "HPC":
         subprocess.call("qsub -v folder=\"{}\" job.sh".format(sim), shell = True)
