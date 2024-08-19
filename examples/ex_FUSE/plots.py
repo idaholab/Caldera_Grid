3c20983ba0e4256bc3fb1b00ddc0ff622814c2ce
Manoj Kumar Cebol Sundarrajan <ManojKumar.CebolSundarrajan@inl.gov>
2024-08-19 12:18:01 -0600

    update plotting outputs

--- examples/ex_FUSE/plots.py
+++ examples/ex_FUSE/plots.py
@@ -7,311 +7,162 @@ Created on Tue Feb 20 13:46:41 2024
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import numpy as np
 import os
-import glob
 import sys
 
 path_to_here = os.path.abspath(os.path.dirname(sys.argv[0]))
 
-output_folder = os.path.join(path_to_here, "outputs")
-figures_folder = os.path.join(path_to_here, "figures")
-power_profiles_figures_folder = os.path.join(figures_folder, "power_profiles_plots")
+caldera_grid_proj_dir = os.path.join(path_to_here, "..", "..")
 
-profile = "total_demand_kW"
+index = 1
+sys.path.insert( index+0, os.path.join( caldera_grid_proj_dir, "./" ) )
+sys.path.insert( index+1, os.path.join( caldera_grid_proj_dir, "./libs" ) )
+sys.path.insert( index+2, os.path.join( caldera_grid_proj_dir, "./source/base" ) )
+sys.path.insert( index+3, os.path.join( caldera_grid_proj_dir, "./source/custom_controls" ) )
+sys.path.insert( index+4, os.path.join( caldera_grid_proj_dir, "./source/ES500" ) )
+sys.path.insert( index+5, os.path.join( caldera_grid_proj_dir, "./source/federates" ) )
+
+from dynamic_price_control.cost_forecaster import TE_cost_forecaster_v3
+
+#----------------------------------
 
 def extract_power_profile_df(folder : str):
     
-    data_folder = os.path.join(output_folder, folder)
-    df = pd.read_csv(os.path.join(data_folder, "real_power_profiles.csv"))
-
-    df = df[(df["simulation_time_hrs"] >= 24) & (df["simulation_time_hrs"] < 48)]
-    df["simulation_time_hrs"] = df["simulation_time_hrs"] % 24.0
+    df = pd.read_csv(os.path.join(folder, "real_power_profiles.csv"))
+    df["sim_time"] = pd.to_datetime((df["simulation_time_hrs"]*3600).round(0), unit='s')
 
     return df
 
+#----------------------------------
 
-def extract_cost_profile_df(folder : str):
-    
-    data_folder = os.path.join(output_folder, folder)
-    df = pd.read_csv(os.path.join(data_folder, "cost_profile.csv"))
+output_folder = os.path.join(path_to_here, "outputs")
+figures_folder = os.path.join(path_to_here, "figures")
 
-    df.columns = [column.split("|")[0].strip() for column in df.columns.to_series()]
+sim_start = 168
+sim_end = 288
 
-    df = df[(df["time"] >= 24) & (df["time"] < 48)]
-    df["time"] = df["time"] % 24.0
+home_start = 204
+home_end = 252
 
-    return df
+work_start = 216
+work_end = 264
 
-def find_ymax(num):
-    
-    y_max = 1
-    while y_max < num:
-        y_max *= 10
-    return y_max
+profile = "total_demand_kW"
 
-#-----------------------------------
-# Solar profiles
-#-----------------------------------
+offset = 204
 
-# Handled in input_generation.py
+figsize = (18, 9)
 
-#-----------------------------------
-# Fossil fuel profiles
-#-----------------------------------
+scenario_range = np.arange(0, 101, 25)
 
-# Handled in input_generation.py
+colors = ['red', 'orangered', 'goldenrod', 'olivedrab', 'seagreen', 'darkgreen']
 
-#-----------------------------------
-# Power profiles
-#-----------------------------------
+home_time = pd.to_datetime((np.arange(home_start, home_end, 0.25)*3600).round(0), unit='s')
+work_time = pd.to_datetime((np.arange(work_start, work_end, 0.25)*3600).round(0), unit='s')
 
-ratio_folders = [os.path.basename(os.path.normpath(folder)) for folder in glob.glob(os.path.join(output_folder, "*"))]
+#----------------------------------
 
-fig_size = (13, 10)
+# Cost Forecaster
 
-x_lims = (0, 24)
-x_ticks = np.linspace(x_lims[0], x_lims[1], 13)
-text_x = 0.5
+cost_forecaster = TE_cost_forecaster_v3(os.path.join("TE_profiles", "base_inputs_folder", "TE_inputs"), ".", False)
 
-# y_lims, y_ticks, and text_y are set based on uncontrolled profile data
+home_forecast = []
+home_actual = []
 
-for ratio_folder in ratio_folders:
+for time_hrs in np.arange(home_start, home_end, 0.25):
+    home_forecast.append(cost_forecaster.get_forecasted_cost_at_time_sec(time_hrs*3600, 0.25*3600))
+    home_actual.append(cost_forecaster.get_actual_cost_at_time_sec(time_hrs*3600, 0.25*3600))
 
-    #-----------------------------------
-    # Uncontrolled
-    #-----------------------------------
+work_forecast = []
+work_actual = []
 
-    df_u = extract_power_profile_df(os.path.join(ratio_folder, "uncontrolled"))
-    
-    fixed_charging_cost_usd_per_kWh = 0.12
-    
-    timestep_hrs = df_u["simulation_time_hrs"].iloc[1] - df_u["simulation_time_hrs"].iloc[0]
-    E_uncontrolled_total = df_u[profile].sum()*timestep_hrs
-    uncontrolled_total_charging_cost = E_uncontrolled_total * fixed_charging_cost_usd_per_kWh
-    
-    y_max = find_ymax(df_u[profile].max())    
-    y_lims = (0, y_max)
-    y_ticks = np.linspace(y_lims[0], y_lims[1], 11)
-    text_y = 0.95*y_max
-    
-    if True:
-        fig, ax1 = plt.subplots(1, 1, figsize = fig_size)
-        ax1.plot(df_u["simulation_time_hrs"], df_u[profile], label = "uncontrolled", color = "red")
-        ax1.set_title("Uncontrolled power profile")
-        ax1.set_xticks(x_ticks)
-        ax1.set_yticks(y_ticks)
-        ax1.set_xlim(x_lims[0], x_lims[1])
-        ax1.set_ylim(y_lims[0], y_lims[1])
-        ax1.set_xlabel('Simulation Time | hrs', labelpad=30)
-        ax1.set_ylabel('Power | kW', labelpad=30)
-        ax1.grid(alpha = 0.5, linestyle='--')
-    
-        plt.text(text_x, text_y, 
-            "Fixed \$/kWh : \${:.3f}".format(fixed_charging_cost_usd_per_kWh) + "\n" +
-            "Uncontrolled \$ total : \${:.3f}".format(uncontrolled_total_charging_cost),
-            ha='left', va='top', fontsize = 'xx-large')
-        
-        ax2 = ax1.twinx()
-        ax2.plot([], [], label = "")
-        ax2.set_ylabel(' ', labelpad=30)
-        ax2.set_yticks([-10, 10])
-        ax2.set_ylim(0, 1)
-        
-        fig.legend()
-        fig.tight_layout()
-        fig_file_name = ratio_folder + "_uncontrolled.png"
-    
-        os.makedirs(power_profiles_figures_folder, exist_ok = True)
-        plt.close(fig)
-        fig.savefig( os.path.join(power_profiles_figures_folder, fig_file_name), dpi = 300)
+for time_hrs in np.arange(work_start, work_end, 0.25):
+    work_forecast.append(cost_forecaster.get_forecasted_cost_at_time_sec(time_hrs*3600, 0.25*3600))
+    work_actual.append(cost_forecaster.get_actual_cost_at_time_sec(time_hrs*3600, 0.25*3600))
 
-    #-----------------------------------
-    # Time of Use
-    #-----------------------------------
-    
-    df_tou = extract_power_profile_df(os.path.join(ratio_folder, "time_of_use"))
-    
-    fixed_charging_cost_tou_usd_per_kWh = 0.08
-    fixed_charging_cost_non_tou_usd_per_kWh = 0.14
-    
-    E_uncontrolled_tou = df_u[(df_u["simulation_time_hrs"] >= 8) & (df_u["simulation_time_hrs"] < 16)][profile].sum()*timestep_hrs
-    E_uncontrolled_non_tou = df_u[(df_u["simulation_time_hrs"] < 8) | (df_u["simulation_time_hrs"] >= 16)][profile].sum()*timestep_hrs
-    
-    E_controlled_tou = df_tou[(df_tou["simulation_time_hrs"] >= 8) & (df_tou["simulation_time_hrs"] < 16)][profile].sum()*timestep_hrs
-    E_controlled_non_tou = df_tou[(df_tou["simulation_time_hrs"] < 8) | (df_tou["simulation_time_hrs"] >= 16)][profile].sum()*timestep_hrs
-    
-    uncontrolled_EV_charging_cost = E_uncontrolled_tou*fixed_charging_cost_tou_usd_per_kWh + E_uncontrolled_non_tou*fixed_charging_cost_non_tou_usd_per_kWh
-    tou_EV_charging_cost = E_controlled_tou*fixed_charging_cost_tou_usd_per_kWh + E_controlled_non_tou*fixed_charging_cost_non_tou_usd_per_kWh
-    
-    if True:
-        fig, ax1 = plt.subplots(1, 1, figsize = fig_size)
-        ax1.plot(df_u["simulation_time_hrs"], df_u[profile], label = "uncontrolled", color = "red")
-        ax1.plot(df_tou["simulation_time_hrs"], df_tou[profile], label = "time_of_use", color = "orange")
-        ax1.set_title("Time of use power profile")
-        ax1.set_xticks(x_ticks)
-        ax1.set_yticks(y_ticks)
-        ax1.set_xlim(x_lims[0], x_lims[1])
-        ax1.set_ylim(y_lims[0], y_lims[1])
-        ax1.set_xlabel('Simulation Time | hrs', labelpad=30)
-        ax1.set_ylabel('Power | kW', labelpad=30)
-        ax1.grid(alpha = 0.5, linestyle='--')
-    
-        plt.text(text_x, text_y, 
-            "TOU period fixed \$/kWh: \${:.3f}".format(fixed_charging_cost_tou_usd_per_kWh) + "\n" +
-            "Non TOU period fixed \$/kWh: \${:.3f}".format(fixed_charging_cost_non_tou_usd_per_kWh) + "\n" +
-            "Uncontrolled \$ total: \${:.3f}".format(uncontrolled_EV_charging_cost) + "\n" +
-            "TOU control \$ total: \${:.3f}".format(tou_EV_charging_cost), 
-            ha='left', va='top', fontsize = 'xx-large')
-        
-        ax2 = ax1.twinx()
-        ax2.plot([], [], label = "")
-        ax2.set_ylabel(' ', labelpad=30)
-        ax2.set_yticks([-10, 10])
-        ax2.set_ylim(0, 1)
-    
-        fig.legend()
-        fig.tight_layout()
-        fig_file_name = ratio_folder + "time_of_use.png"
-    
-        os.makedirs(power_profiles_figures_folder, exist_ok = True)
-        plt.close(fig)
-        fig.savefig( os.path.join(power_profiles_figures_folder, fig_file_name), dpi = 300)
+#----------------------------------
 
-    #-----------------------------------
-    # Good Forecast Scenarios
-    #-----------------------------------
-    
-    high_folder = "good_forecast"
-    low_folders = ["linear", "steep_cubic", "inverse_s"]
-    
-    timestep_hrs = 15*50/3600
-    
-    for low_folder in low_folders:
+# Home Charging
 
-        files = glob.glob(os.path.join(output_folder, ratio_folder, high_folder, low_folder, "*"))
-    
-        all_profiles = [] 
-        for file in files[:]:
-        
-            scenario = os.path.split(file)[1]
-            
-            df = extract_power_profile_df(os.path.join(ratio_folder, high_folder, low_folder, scenario))
-            df2 = extract_cost_profile_df(os.path.join(ratio_folder, high_folder, low_folder, scenario))
-        
-            avg_charging_cost = df2["actual_cost"].mean()
-        
-            E_uncontrolled = np.mean(df_u[profile].to_numpy().reshape(-1, 15), axis=1) * timestep_hrs # average power for every 15 timesteps then converty to energy
-            uncontrolled_total_charging_cost = np.sum(E_uncontrolled * df2["actual_cost"])
-        
-            E_tou_controlled = np.mean(df_tou[profile].to_numpy().reshape(-1, 15), axis=1) * timestep_hrs
-            tou_total_charging_cost = np.sum(E_tou_controlled * df2["actual_cost"])
-        
-            E_TE_control = np.mean(df[profile].to_numpy().reshape(-1, 15), axis=1) * timestep_hrs
-            TE_total_charging_cost = np.sum(E_TE_control * df2["actual_cost"])
-            
-        
-            all_profiles.append((scenario, df[profile]))
-            
-            if True:
-                fig, ax1 = plt.subplots(figsize = fig_size)
-                ax1.plot(df_u["simulation_time_hrs"], df_u[profile], label = "uncontrolled", color = "red", alpha = 0.3)
-                ax1.plot(df_tou["simulation_time_hrs"], df_tou[profile], label = "time_of_use", color = "orange", alpha = 0.3)
-                ax1.plot(df["simulation_time_hrs"], df[profile], label = "TE_control", color = "blue", linewidth = 2)
-        
-                ax1.set_title(high_folder + " " + low_folder + " " + scenario + " power profile")
-                ax1.set_xticks(x_ticks)
-                ax1.set_yticks(y_ticks)
-                ax1.set_xlim(x_lims[0], x_lims[1])
-                ax1.set_ylim(y_lims[0], y_lims[1])
-                ax1.set_xlabel('Simulation Time | hrs', labelpad=30)
-                ax1.set_ylabel('Power | kW', labelpad=30)
-                ax1.grid(alpha = 0.5, linestyle='--')
-        
-                plt.text(text_x, text_y, 
-                "average \$/kWh: \${:.3f}".format(avg_charging_cost) + "\n" +
-                "Uncontrolled \$ total: \${:.3f}".format(uncontrolled_total_charging_cost) + "\n" +
-                "TOU control \$ total: \${:.3f}".format(tou_total_charging_cost) + "\n" +
-                "TE control \$ total: \${:.3f}".format(TE_total_charging_cost), 
-                ha='left', va='top', fontsize = 'xx-large')
-                
-                ax2 = ax1.twinx()
-                ax2.plot(df2["time"], df2["forecasted_cost"], color = "purple", label = "forecasted and actual cost", linestyle='dashed', linewidth = 2)
-                ax2.set_ylabel('Cost | $/kWh', labelpad=30)
-                ax2.set_ylim(0.05, 0.25)
-            
-                fig.legend()
-                fig.tight_layout()
-                fig_file_name = ratio_folder + "_" + high_folder + "_" + low_folder + "_" + scenario + ".png"
-                plt.close(fig)
-                fig.savefig( os.path.join(power_profiles_figures_folder, fig_file_name), dpi = 300)
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=figsize)
 
+ax1.step(home_time, home_forecast, label = "forecast $/kWh", color = "brown", where = 'post' )
+ax1.step(home_time, home_actual, label = "actual $/kWh", color = "black", where = 'post' )
 
-    #-----------------------------------
-    # Bad Forecast Scenarios
-    #-----------------------------------
+ax1.set_xlim(home_time[0], home_time[-1])
+ax1.xaxis.set_major_locator(mdates.HourLocator(interval = 6))
+ax1.xaxis.set_major_formatter(mdates.DateFormatter('%I:%M %p'))
 
-    high_folder = "bad_forecast"
-    low_folders = ["linear", "steep_cubic", "inverse_s"]
-    
-    for low_folder in low_folders:
+ax1.set_ylim([0.05, 0.25])
+ax1.set_yticks(np.arange(0.05, 0.26, 0.01))
+ax1.set_ylabel('Cost | $/kWh', labelpad=30)
 
-        files = glob.glob(os.path.join(output_folder, ratio_folder, high_folder, low_folder, "*"))
+ax1.grid(True)
+ax1.legend()
 
-        all_profiles = [] 
-        for file in files[:]:
-        
-            scenario = os.path.split(file)[1]
-            
-            df = extract_power_profile_df(os.path.join(ratio_folder, high_folder, low_folder, scenario))
-            df2 = extract_cost_profile_df(os.path.join(ratio_folder, high_folder, low_folder, scenario))
-        
-            avg_forecasted_charging_cost = df2["forecasted_cost"].mean()
-            avg_actual_charging_cost = df2["actual_cost"].mean()
-        
-            E_uncontrolled = np.mean(df_u[profile].to_numpy().reshape(-1, 15), axis=1) * timestep_hrs # average power for every 15 timesteps then converty to energy
-            uncontrolled_total_charging_cost = np.sum(E_uncontrolled * df2["actual_cost"])
-        
-            E_tou_controlled = np.mean(df_tou[profile].to_numpy().reshape(-1, 15), axis=1) * timestep_hrs
-            tou_total_charging_cost = np.sum(E_tou_controlled * df2["actual_cost"])
-        
-            E_TE_control = np.mean(df[profile].to_numpy().reshape(-1, 15), axis=1) * timestep_hrs
-            TE_total_charging_cost = np.sum(E_TE_control * df2["actual_cost"])
-        
-            
-            all_profiles.append((scenario, df[profile]))
-            
-            if True:
-                fig, ax1 = plt.subplots(figsize = fig_size)
-                ax1.plot(df_u["simulation_time_hrs"], df_u[profile], label = "uncontrolled", color = "red", alpha = 0.3)
-                ax1.plot(df_tou["simulation_time_hrs"], df_tou[profile], label = "time_of_use", color = "orange", alpha = 0.3)
-                ax1.plot(df["simulation_time_hrs"], df[profile], label = "TE_control", color = "blue", linewidth = 2)
-        
-                ax1.set_title(high_folder + " " + low_folder + " " + scenario + " power profile")
-                ax1.set_xticks(x_ticks)
-                ax1.set_yticks(y_ticks)
-                ax1.set_xlim(x_lims[0], x_lims[1])
-                ax1.set_ylim(y_lims[0], y_lims[1])
-                ax1.set_xlabel('Simulation Time | hrs', labelpad=30)
-                ax1.set_ylabel('Power | kW', labelpad=30)
-                ax1.grid(alpha = 0.5, linestyle='--')
-        
-                plt.text(text_x, text_y, 
-                "Average forecasted \$/kWh: \${:.3f}".format(avg_forecasted_charging_cost) + "\n" +
-                "Average actual \$/kWh: \${:.3f}".format(avg_actual_charging_cost) + "\n" +
-                "Uncontrolled \$ total: \${:.3f}".format(uncontrolled_total_charging_cost) + "\n" +
-                "TOU control \$ total: \${:.3f}".format(tou_total_charging_cost) + "\n" +
-                "TE control \$ total: \${:.3f}".format(TE_total_charging_cost), 
-                ha='left', va='top', fontsize = 'xx-large')
-            
-                ax2 = ax1.twinx()
-                ax2.plot(df2["time"], df2["forecasted_cost"], color = "chocolate", label = "forecasted cost", linestyle='dashed', linewidth = 2)
-                ax2.plot(df2["time"], df2["actual_cost"], color = "teal", label = "actual cost", linestyle='dashed', linewidth = 2)
-                ax2.set_ylabel('Cost | $/kWh', labelpad=30)
-                ax2.set_ylim(0.05, 0.25)
-            
-                fig.legend()
-                fig.tight_layout()
-                fig_file_name = ratio_folder + "_" + high_folder + "_" + low_folder + "_" + scenario + ".png"
-                plt.close(fig)
-                fig.savefig( os.path.join(power_profiles_figures_folder, fig_file_name), dpi = 300)
+for i, val in enumerate(scenario_range):
+    scenario = "home_dynamic_{}".format(val)
+    df = extract_power_profile_df(os.path.join(output_folder, scenario))
+    df_sub = df[(df["simulation_time_hrs"] >= home_start) & (df["simulation_time_hrs"] < home_end)]
+    ax2.plot(df_sub["sim_time"], df_sub[profile], label = scenario, color = colors[i])
+
+ax2.set_xlim(df_sub["sim_time"].iloc[0], df_sub["sim_time"].iloc[-1])
+ax2.xaxis.set_major_locator(mdates.HourLocator(interval = 6))
+ax2.xaxis.set_major_formatter(mdates.DateFormatter('%I:%M %p'))
+
+ax2.set_xlabel('Time | hrs', labelpad=30)
+ax2.set_ylabel('Power | kW', labelpad=30)
+
+ax2.grid(True)
+ax2.legend()
+
+fig.tight_layout(rect=[0, 0.03, 1, 0.95])
+fig.suptitle("Home Charging")
+
+#os.makedirs(power_profiles_figures_folder, exist_ok = True)
+fig.savefig( os.path.join(figures_folder, "home.png"), dpi = 300)
+
+#----------------------------------
+
+# Work Charging
+
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=figsize)
+
+ax1.step(work_time, work_forecast, label = "forecast $/kWh", color = "brown", where = 'post' )
+ax1.step(work_time, work_actual, label = "actual $/kWh", color = "black", where = 'post' )
+
+#+ pd.Timedelta(days=1)
+ax1.set_xlim(work_time[0], work_time[-1])
+ax1.xaxis.set_major_locator(mdates.HourLocator(interval = 6))
+ax1.xaxis.set_major_formatter(mdates.DateFormatter('%I:%M %p'))
+
+ax1.set_ylim([0.05, 0.25])
+ax1.set_yticks(np.arange(0.05, 0.26, 0.01))
+ax1.set_ylabel('Cost | $/kWh', labelpad=30)
+
+ax1.grid(True)
+ax1.legend()
+
+for i, val in enumerate(scenario_range):
+    scenario = "work_dynamic_{}".format(val)
+    df = extract_power_profile_df(os.path.join(output_folder, scenario))
+    df_sub = df[(df["simulation_time_hrs"] >= work_start) & (df["simulation_time_hrs"] < work_end)]
+    ax2.plot(df_sub["sim_time"], df_sub[profile], label = scenario, color = colors[i])
+
+ax2.set_xlim(df_sub["sim_time"].iloc[0], df_sub["sim_time"].iloc[-1])
+ax2.xaxis.set_major_locator(mdates.HourLocator(interval = 6))
+ax2.xaxis.set_major_formatter(mdates.DateFormatter('%I:%M %p'))
+
+ax2.set_xlabel('Time | hrs', labelpad=30)
+ax2.set_ylabel('Power | kW', labelpad=30)
+
+ax2.grid(True)
+ax2.legend()
+
+fig.tight_layout(rect=[0, 0.03, 1, 0.95])
+fig.suptitle("Work Charging")
+
+#os.makedirs(power_profiles_figures_folder, exist_ok = True)
+fig.savefig( os.path.join(figures_folder, "work.png"), dpi = 300)
