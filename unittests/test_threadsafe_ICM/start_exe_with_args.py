091ae8b31186f562e098914cf89f6bc40eef846b
Manoj Kumar Cebol Sundarrajan <ManojKumar.CebolSundarrajan@inl.gov>
2025-01-02 11:21:37 -0700

    unittest folder to test threadsafe ICM

--- unittests/test_threadsafe_ICM/start_exe_with_args.py
+++ unittests/test_threadsafe_ICM/start_exe_with_args.py
@@ -7,9 +7,9 @@ path_to_here = os.path.abspath(os.path.dirname(sys.argv[0]))
 path_to_libs = os.path.join( path_to_here, "../../" )
 input_directory = os.path.join( path_to_here, "inputs/" )
 output_directory = os.path.join( path_to_here, "outputs/" )
-timestep = 10
+timestep = 60
 starttime = 0.5*3600
-endtime = 72*3600
+endtime = 12*3600
 
 command = [
     "python", os.path.join( path_to_here, "../../start_execution.py" ),
